db0639f2bacf36cc76dbb1a1827d889385ddd242
Manu Maheshwari <manum@Manus-Air.lan>
2024-11-21 22:40:03 -0600

    Adding initial code for Chubby server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,4 +1,5 @@
 #include "Chubby.grpc.pb.h"
+#include "rocksdb_wrapper.h"
 #include "Chubby.pb.h"
 #include "paxos_server.h"
 #include <chrono>
@@ -22,10 +23,116 @@ using grpc::ServerContext;
 using grpc::Status;
 
 using chubby::Chubby;
+using chubby::AcquireLockRequest;
+using chubby::AcquireLockResponse;
+using chubby::TryAcquireLockRequest;
+using chubby::TryAcquireLockResponse;
+using chubby::LockingMode;
+
+class KeyLock {
+    std::unordered_map<std::string, std::shared_ptr<std::mutex>> lock_map;
+    std::mutex map_mutex; 
+
+public:
+    void lock(std::string key) {
+        std::shared_ptr<std::mutex> key_mutex;
+        {
+            std::lock_guard<std::mutex> lock(map_mutex);
+            if (lock_map.find(key) == lock_map.end()) {
+                lock_map[key] = std::make_shared<std::mutex>();
+            }
+            key_mutex = lock_map[key];
+        }
+        key_mutex->lock();
+    }
+
+    void unlock(std::string key) {
+        std::shared_ptr<std::mutex> key_mutex;
+        {
+            std::lock_guard<std::mutex> lock(map_mutex);
+            key_mutex = lock_map[key];
+        }
+        key_mutex->unlock();
+    }
+};
 
 class ChubbyImpl final : public Chubby::Service {
+private:
+
+  RocksDBWrapper chubby_db;
+  KeyLock key_lock;
+  std::set<std::string> client_session_map; //Tracks if a client currently has session with the Chubby master, need not persist, recovery through client 
+
   public:
-  ChubbyImpl() {}
+  
+  ChubbyImpl(std::string db_path,std::size_t cache_size) : 
+    chubby_db(db_path,cache_size){
+  
+  }
+
+  Status AcquireLock(ServerContext *context, const AcquireLockRequest *request, AcquireLockResponse *response) override {
+
+    key_lock.lock(request->path()); //So that other key paths are not locked 
+
+    std::string request_locking_mode = LockingMode_Name(request->locking_mode());
+
+    if(client_session_map.find(request->client_id()) == client_session_map.end()){ //If machine comes back after getting killed for some time, should this be persisted?
+
+      response->set_success(false);
+      response->set_error_message("NO_CLIENT_SESSION");
+
+    }else{
+      std::string value;
+      bool key_found = chubby_db.Get(request->path(),value);
+
+      if(key_found){
+        if(request_locking_mode == "SHARED"){
+          if(value == "FREE"){
+            //Need to add client_id also to the value?
+            chubby_db.Put(request->path(),request_locking_mode,value);
+            response->set_success(true);
+            response->set_error_message("NO_ERROR");
+          }else if(value == "SHARED"){
+            //If client id needs to be added then we need to put to chubby_db
+            response->set_success(true);
+            response->set_error_message("NO_ERROR");
+          }else if(value == "EXCLUSIVE"){
+            response->set_success(false);
+            response->set_error_message("EXCLUSIVE_LOCK_HELD");
+          }
+        }else if(request_locking_mode == "EXCLUSIVE"){
+          if(value == "FREE"){
+            chubby_db.Put(request->path(),request_locking_mode,value);
+            response->set_success(true);
+            response->set_error_message("NO_ERROR");
+          }else if(value == "SHARED"){
+            response->set_success(false);
+            response->set_error_message("SHARED_LOCK_HELD");
+          }else if(value == "EXCLUSIVE"){
+            response->set_success(false);
+            response->set_error_message("EXCLUSIVE_LOCK_HELD");
+          }
+        }
+      }else{
+        //Need to add client_id also to the value?
+        std::string old_value;
+        chubby_db.Put(request->path(),request_locking_mode,old_value);
+        response->set_success(true);
+        response->set_error_message("NO_ERROR");
+      }
+    }
+
+    key_lock.unlock(request->path());
+
+    return Status::OK;
+      
+  }
+
+  Status TryAcquireLock(ServerContext *context, const TryAcquireLockRequest *request, TryAcquireLockResponse *response) override {
+
+    return Status::OK;
+  }
+
 };
 
 void RunServer(std::string &server_address, int total_servers,
@@ -33,9 +140,9 @@ void RunServer(std::string &server_address, int total_servers,
   int host_port = std::stoi(server_address.substr(server_address.find(":") + 1,
                                              server_address.size()));
   
-  ChubbyImpl chubby_service;
 
   size_t cache_size = 20 * 1024 * 1024; // 20MB cache
+  ChubbyImpl chubby_service("/chubby",cache_size);
   PaxosImpl paxos_service(total_servers, "/rocksdb", cache_size, server_address);
 
   ServerBuilder builder;
